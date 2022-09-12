06b211af7292601b219edc876b15266210f46b8b
Lemonsprig <ianselbyuk@gmail.com>
2022-09-12 21:54:35 +0100

    Added function to revers strings. All automated tests passing.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringArray =  string.split("");
+    stringArray.reverse();
+    stringReversed = stringArray.join('');
+    return stringReversed;
 };
 
 // Do not edit below this line
