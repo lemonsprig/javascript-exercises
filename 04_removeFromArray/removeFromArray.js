098a51a15b0590fbf7ed11a0940bc9d75f385000
Lemonsprig <ianselbyuk@gmail.com>
2022-09-13 15:16:24 +0100

    Added code for removeFromArray all tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...mindex) {
+    let changedArray = [];
+    for(i=0; i < myArray.length; i++) {
+        if (mindex.includes(myArray[i])) {
 
+        } else {
+            changedArray.push(myArray[i]);
+        }
+    }
+    return changedArray;
 };
 
 // Do not edit below this line
