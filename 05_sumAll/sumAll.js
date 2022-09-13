deba232df1c216c82c55a36ec6d60ec31a6e2a4a
Lemonsprig <ianselbyuk@gmail.com>
2022-09-13 20:57:02 +0100

    Added code for the sumAll excercise. All tests passed. re-affirmed that strict comparison operators for >= or <= do not exist. i.e. <== or >==

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(Num1, Num2) {
+let total = 0;
+    if ((Num1 < 0 || typeof(Num1) !== "number") || (Num2 < 0 || typeof(Num2) !=="number")){
+       return "ERROR"; 
+    } 
+    else if (Num1 > Num2) {
+        let tmpNum = Num1;
+        Num1 = Num2
+        Num2 = tmpNum
+    }
 
+    for (i=Num1; i<=Num2; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
