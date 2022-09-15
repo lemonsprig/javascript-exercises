33e251adfb3848fa8353deebf9fa35512d0a4d24
Lemonsprig <ianselbyuk@gmail.com>
2022-09-15 13:32:05 +0100

    Added code to the return the value of the nth fibonacci number. All tests passed.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS"
+    } else {
+        let value1 = 1;
+        let value2 = 1;
+        let value = 1;
+        num = Number(num);
+        for (i=3; i<=num; i++){
+            value = value1 + value2;
+            value1 = value2;
+            value2 = value;
+        }
+    return value;
+    }
 };
 
 // Do not edit below this line
