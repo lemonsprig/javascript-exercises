4ce63203896d8d1d834be6d860ec86c9068cf5f0
Lemonsprig <ianselbyuk@gmail.com>
2022-09-15 12:02:14 +0100

    Added code to check for palindromes. All tests passed. This was an interesting challenge and I had to explore the use of regular expressions.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+ text = text.toLowerCase()
+ text = text.replace(/[^A-Za-z]/g,'')
+ reverseText = text.split('').reverse().join('')
+ if (text === reverseText) {
+    return true;
+ } 
+ else 
+ {
+    return false;
+ }
 };
 
 // Do not edit below this line
