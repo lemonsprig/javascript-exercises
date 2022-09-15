c4366a422251ac9c7cc41d4d73fdf8ea700d8700
Lemonsprig <ianselbyuk@gmail.com>
2022-09-15 21:36:43 +0100

    Added code to find the oldest person from an array or people. All tests passed.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    oldestPersonAge = 0;
+    oldestPersonIndex = 0;
+    let age = 0;
+    for (i=0; i< people.length; i++) {
+        if (people[i].yearOfDeath == undefined){
+            let today = new Date();
+            age = today.getFullYear() - people[i].yearOfBirth;
+        } else {
+            age = people[i].yearOfDeath - people[i].yearOfBirth
+        }
+        if (age > oldestPersonAge) {
+            oldestPersonAge = age;
+            oldestPersonIndex = i;
+        }
+    }
+    return people[oldestPersonIndex];
 };
 
 // Do not edit below this line
