8593e3bb2d6b3969e51b4bba2c8cb5fc9cf80f31
Lemonsprig <ianselbyuk@gmail.com>
2022-09-15 10:37:12 +0100

    Added calculator functions. All tests passed.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sum = 0;
+  for(const number of numbers){
+    sum += number;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  product = numbers[0]
+  for (i=1; i < numbers.length; i++){
+    product *= numbers[i] 
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) {
+    return 1;
+  } 
+  else {
+    let product = 1;
+	  for(i=1; i <= number; i++) {
+      product *= i ;
+    }
+    return product;
+  }
 };
 
 // Do not edit below this line
