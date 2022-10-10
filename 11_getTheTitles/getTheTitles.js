d4b9aa8c2d474feddb2389dc2eacd926ffd9d4da
Lemonsprig <ianselbyuk@gmail.com>
2022-10-10 11:19:38 +0100

    Refactored the code to use array.map

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,9 +1,12 @@
 const getTheTitles = function(books) {
-    let titles = []
-    for (book of books) {
-        titles.push(book.title);
-    }
-    return titles;
+    // let titles = []
+    // for (book of books) {
+    //     titles.push(book.title);
+    // }
+    // return titles;
+
+    return books.map(book => book.title)
+
 };
 
 // Do not edit below this line
